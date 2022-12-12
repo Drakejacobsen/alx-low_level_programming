d59ba1182a23b6972f7a87b1ef7d104bd55e53e6
Drakejacobsen <jacobsenlars13@gmail.com>
2022-12-12 10:57:17 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,6 @@
  * different combinations of two digits
  * Numbers must be separated by ,, followed by a space 
  * The two digits must be different
- * 1 and 10 are considered the same combination of the two digits 0 and 1
  * Print only the smallest combination of two digits
  * You can only use the putchar function
  * (every other function (printf, puts, etc...) is forbidden)
