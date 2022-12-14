d141d53d9da26ffc9c51725450b36374ac7fdd81
Drakejacobsen <jacobsenlars13@gmail.com>
2022-12-14 08:18:53 -0800

    Main

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -34,8 +34,8 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar('0' + m2);
 					_putchar('0' + m1);
-					_putchar('\n')
-						m1++;
+					_putchar('\n');
+					m1++;
 				}
 				m2++;
 			}
