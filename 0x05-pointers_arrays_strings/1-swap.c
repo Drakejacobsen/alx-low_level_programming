d15cae3d1d20cc93c5525d315ae989b6633e6a28
Drakejacobsen <jacobsenlars13@gmail.com>
2022-12-18 23:34:34 -0800

    Main

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * swap_imt - swaps the values of two integers
+ * using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+ */
+
+void swap int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
