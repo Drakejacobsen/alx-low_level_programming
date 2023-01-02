961e2aa25e3cc3a0daddd79915c4ceb730680025
Drakejacobsen <jacobsenlars13@gmail.com>
2023-01-02 02:36:10 -0800

    main

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,12 +16,12 @@
  * Return: A pointer to the filled memory area @s.
  *
  */
-void *_memset(void *S, int c, size_t n)
+void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
 
-	for (index =0; index < n; index++)
+	for (index = 0; index < n; index++)
 		memory[index] = value;
 	return (memory);
 }
