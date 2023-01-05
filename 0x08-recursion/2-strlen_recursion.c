712e5d3021c10a51ecd706e5c90e2a227f54a6df
Drakejacobsen <jacobsenlars13@gmail.com>
2023-01-05 00:44:42 -0800

    main

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string.
+ * @s:pointer to string.
+ *
+ * Return: length of strin.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
