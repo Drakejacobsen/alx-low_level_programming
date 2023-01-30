07993b985919d1ac6b08ed86ec57cec60afacc1a
Drakejacobsen <jacobsenlars13@gmail.com>
2023-01-30 03:08:07 -0800

    lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - Calculate the numebr of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ */
+size_t listint_len(const listint_t *h)
+{
+	comst listint_t *tp;
+	unsigned int cnr = 0;
+
+	tp = h;
+	while (tp)
+	{
+		cnr++;
+		tp = tp->next;
+	}
+	return (cnr);
+}
