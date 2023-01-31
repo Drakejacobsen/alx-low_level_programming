58b10f65d7ae782df046c876112fa0385acd247f
Drakejacobsen <jacobsenlars13@gmail.com>
2023-01-30 23:58:14 -0800

    lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for project
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} list int_t;
+#include <stdlib.h>
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -31,3 +18,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+#endif
